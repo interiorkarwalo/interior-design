cf1623cb4e0ffe42400d95ddbfa8c92a6b45aa3a
deepak1051 <lightyagamigoku@gmail.com>
2023-03-14 21:24:58 +0530

    remove s

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://api.interiorkarwalo.com/api/v1/admin',
+  baseURL: 'http://api.interiorkarwalo.com/api/v1/admin',
 });
 
 export default instance;
