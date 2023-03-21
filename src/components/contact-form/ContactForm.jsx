f3f41457d5787f58b7d1f54e82523ec06f4ef34e
deepak1051 <lightyagamigoku@gmail.com>
2023-03-21 12:28:35 +0530

    head tag

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -40,29 +40,28 @@ const ContactForm = () => {
               <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 ADDRESS
               </h2>
-              <p class="mt-1">
-                Photo booth tattooed prism, portland taiyaki hoodie neutra
-                typewriter
-              </p>
+              <p class="mt-1">Geeta Nagar, Rishikesh, Uttarakhand, 249201</p>
             </div>
             <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a class="text-indigo-500 leading-relaxed">example@email.com</a>
+              <a class="text-indigo-500 leading-relaxed">
+                info@pacifencesolutions.com
+              </a>
               <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p class="leading-relaxed">123-456-7890</p>
+              <p class="leading-relaxed">+1888-297-6869</p>
             </div>
           </div>
         </div>
         <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-            Feedback
+            Contact us
           </h2>
           <p class="leading-relaxed mb-5 text-gray-600">
-            Post-ironic portland shabby chic echo park, banjo fashion axe
+            to get your dream home
           </p>
           <div class="relative mb-4">
             <label for="name" class="leading-7 text-sm text-gray-600">
@@ -97,7 +96,7 @@ const ContactForm = () => {
             ></textarea>
           </div>
           <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Button
+            Submit
           </button>
           <p class="text-xs text-gray-500 mt-3">
             Chicharrones blog helvetica normcore iceland tousled brook viral
