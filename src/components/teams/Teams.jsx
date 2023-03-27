0dab986b2d516b75dbbd8a675c15a9fa63c13c8a
AnilDaal <anildaal7@gmail.com>
2023-03-27 10:13:50 +0530

    final release

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import './teams.css';
+import React from "react";
+import "./teams.css";
 const Teams = () => {
   return (
     <section class="text-gray-600 body-font">
@@ -21,13 +21,73 @@ const Teams = () => {
               <img
                 alt="team"
                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                src="https://media.licdn.com/dms/image/D4D03AQG06-g1FELWvQ/profile-displayphoto-shrink_400_400/0/1673530992467?e=1684972800&v=beta&t=hxxQPuPA4MWkctGFu2NbJf7xB439BbvVwabu9cIgkEk"
+                src="https://media.licdn.com/dms/image/C4D03AQHeTxm3Wac8AQ/profile-displayphoto-shrink_200_200/0/1661252289902?e=1685577600&v=beta&t=Hle4bT7_IWtVwbrFVSWJkOBk7IDcLFsYlT2s11lzzqM"
               />
               <div class="flex-grow sm:pl-8">
                 <h2 class="title-font font-medium text-lg text-gray-900">
-                  Anil Daal
+                  Kushank Kashyap
                 </h2>
-                <h3 class="text-gray-500 mb-3">Senior Interior Designer</h3>
+                <h3 class="text-gray-500 mb-3">Interior Designer</h3>
+                <p class="mb-4">
+                  DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                  pack vaporware.
+                </p>
+                <span class="inline-flex">
+                  <a class="text-gray-500">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a class="ml-2 text-gray-500">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a class="ml-2 text-gray-500">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/2">
+            <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+              <img
+                alt="team"
+                class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                src="https://media.licdn.com/dms/image/C4E03AQHUtK45wpEHTA/profile-displayphoto-shrink_200_200/0/1606631902829?e=1685577600&v=beta&t=aWBO4OrPqSOtWf39dqugNMpMeUgeHZkOkhAiBzcTnD4"
+              />
+              <div class="flex-grow sm:pl-8">
+                <h2 class="title-font font-medium text-lg text-gray-900">
+                  Additya Saxena
+                </h2>
+                <h3 class="text-gray-500 mb-3">Designer</h3>
                 <p class="mb-4">
                   DIY tote bag drinking vinegar cronut adaptogen squid fanny
                   pack vaporware.
@@ -141,73 +201,13 @@ const Teams = () => {
               <img
                 alt="team"
                 class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                src="https://media.licdn.com/dms/image/C4D03AQFE6Xk_l-k_UA/profile-displayphoto-shrink_400_400/0/1663902081197?e=1684972800&v=beta&t=ULDRYJ8xTx-eCw-QR94322q3qoQ47Lj_uAQt3sFzXIE"
+                src="https://media.licdn.com/dms/image/D4D03AQG06-g1FELWvQ/profile-displayphoto-shrink_400_400/0/1673530992467?e=1684972800&v=beta&t=hxxQPuPA4MWkctGFu2NbJf7xB439BbvVwabu9cIgkEk"
               />
               <div class="flex-grow sm:pl-8">
                 <h2 class="title-font font-medium text-lg text-gray-900">
-                  Priyanshu Chauhan
+                  Anil Daal
                 </h2>
-                <h3 class="text-gray-500 mb-3">Interior Designer</h3>
-                <p class="mb-4">
-                  DIY tote bag drinking vinegar cronut adaptogen squid fanny
-                  pack vaporware.
-                </p>
-                <span class="inline-flex">
-                  <a class="text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a class="ml-2 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a class="ml-2 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div>
-            </div>
-          </div>
-          <div class="p-4 lg:w-1/2">
-            <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-              <img
-                alt="team"
-                class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                src="/deepak-image.jpeg"
-              />
-              <div class="flex-grow sm:pl-8">
-                <h2 class="title-font font-medium text-lg text-gray-900">
-                  Deepak Panwar
-                </h2>
-                <h3 class="text-gray-500 mb-3">Designer</h3>
+                <h3 class="text-gray-500 mb-3">Senior Interior Designer</h3>
                 <p class="mb-4">
                   DIY tote bag drinking vinegar cronut adaptogen squid fanny
                   pack vaporware.
